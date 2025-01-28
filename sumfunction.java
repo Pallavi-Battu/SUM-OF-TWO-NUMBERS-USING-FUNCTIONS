2e3b7fedd9ed0887cc11713a6931eb4ea3f97018
Pallavi-Battu <pallavibattu5@gmail.com>
2025-01-28 15:48:55 +0530

    Add files via upload

---
+++ sumfunction.java
@@ -0,0 +1,14 @@
+
+public class sumfunction {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int result = sum(num1, num2);
+        System.out.println("The sum is: " + result);
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
+
